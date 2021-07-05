cd82edb05b26e9822f112566ba124db0d4f70a31
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-05 18:35:13 +0530

    quiz

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Text('thsi si my app'),
+      ),
+    );
+  }
+}
